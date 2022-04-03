265f8ed2249f50f4a0079e830bf97e07fbf6a6a2
Menu-K <menushan.karunakaran@gmail.com>
2022-04-02 21:25:10 -0400

    Initial commit

---
+++ Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/contacts";
+export const RemoteURI = "mongodb+srv://menu:EnSweOqzPK2eHxck@cluster0.fmyjp.mongodb.net/contacts?retryWrites=true&w=majority"
+export const HostName = "MongoDB Atlas";
+export const SessionSecret = "WEBD6201SessionSecret";
